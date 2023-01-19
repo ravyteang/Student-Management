54487ba7451f856b1c16e73b04703a5a93be40ca
ravyteang <115539750+ravyteang@users.noreply.github.com>
2023-01-19 12:37:57 +0700

    Update file.sh

--- file.sh
+++ file.sh
@@ -3,6 +3,6 @@ sudo apt-get -y update
 sudo apt install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "Hello world from me!!" /var/www/html/index.html
+echo "Hello world from me!!" > /var/www/html/index.html
 sudo systemctl restart apache2
 
